eb35b879dd7d6cd0830de7681bad0dba1ebb1828
kiran kumar chandupatla <kchandupatla@accela.com>
2019-02-01 19:21:54 +0530

    commit all files

---
+++ Chrome_LARA_Ferrari UI_Form Designer_AA_New_AppD Headers/Lib-View.c
@@ -0,0 +1,13 @@
+//   *****************************************************************************************************************************************
+//   ****   PLEASE NOTE: This is a READ-ONLY representation of the actual script. For editing please press the "Develop Script" button.   ****
+//   *****************************************************************************************************************************************
+
+Library_AV2()
+{
+	truclient_step("1", "Function NewUI", "snapshot=AV2_1.inf");
+	{
+		truclient_step("1.1", "Click on Switch New UI link", "snapshot=AV2_1.1.inf");
+	}
+
+	return 0;
+}
